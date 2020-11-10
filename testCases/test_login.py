a968d49ccb52b5ed65bded2094348e96685978c5
constantinpri <pricochi.constantin@gmail.com>
2020-11-09 21:28:23 -0800

    added test_case-001

---
+++ testCases/test_login.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium import webdriver
+from pageObjects.LoginPage import LoginPage
+
+
+class Test_001_Login:
+    basedURL = "https://admin-demo.nopcommerce.com/"
+    username = "admin@yourstore.com"
+    password = "admin"
+
+    def test_homePageTitle(self, setup):
+        self.driver = setup
+        self.driver.get(self.basedURL)
+        act_title = self.driver.title
+        self.driver.close()
+        if act_title == "Your store. Login":
+           assert True
+        else:
+            assert False
+
+    def test_login(self, setup):
+        self.driver = setup
+        self.driver.get(self.basedURL)
+        self.lp = LoginPage(self.driver)
+        self.lp.setUserName(self.username)
+        self.lp.setPassword(self.password)
+        self.lp.clickLogin()
+        act_title = self.driver.title
+        self.driver.close()
+        if act_title == "Dashboard / nopCommerce administration":
+            assert True
+        else:
+            assert False
+
