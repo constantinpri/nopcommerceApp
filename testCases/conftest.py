2813c0cafebd270042142805e7d17277b797f753
constantinpri <pricochi.constantin@gmail.com>
2020-11-11 06:48:18 -0800

    added new files

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -3,6 +3,38 @@ import pytest
 
 
 @pytest.fixture()
-def setup():
-    driver = webdriver.Chrome()
+def setup(browser):
+    if browser == 'chrome':
+       driver = webdriver.Chrome()
+       print("Launching Chrome browser.......")
+    elif browser == 'firefox':
+        driver = webdriver.Firefox()
+        print("Launching FireFox browser........")
+
+    else:
+        driver = webdriver.Chrome()
     return driver
+
+
+
+def pytest_addoption(parser):   # This will get the value from CLI /hooks
+    parser.addoption("--browser")
+
+@pytest.fixture()
+def browser(request): # This will return the browser value to setup method
+    return request.config.getoption("--browser")
+
+################## Pytest HTML Report ######################
+
+#It is hook for adding enviroment info to HTML Report
+def pytest_configure(config):
+    config._metadata['Project Name'] = 'nop Commerce'
+    config._metadata['Module Name'] = 'Customers'
+    config._metadata['Tester'] = 'Constantin'
+
+# It is hook for delete /Modify Enviroment info to HTML report
+
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    metadata.pop("JAVA_HOME", None)
+    metadata.pop("Plugins", None)
