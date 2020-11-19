5a88fef159dec595a1b383efcc7fedfe9d20ed5c
constantinpri <pricochi.constantin@gmail.com>
2020-11-18 21:31:12 -0800

    added test_searchCustomerByEmail.py

--- pageObjects/SearchCustomerPage.py
+++ pageObjects/SearchCustomerPage.py
@@ -1,9 +1,12 @@
+from selenium import webdriver
+
 class SearchCustomer():
     # Add customer Page
     txtEmail_id = "SearchEmail"
     txtFirstName_id="SearchFirstName"
     txtLastName_id="SearchLastName"
     btnSearch_id="search-customers"
+
     tblSearchResults_xpath="//table[@role='grid']"
     table_xpath="//table[@id='customers-grid']"
     tableRows_xpath="//table[@id='customers-grid']//tbody/tr"
